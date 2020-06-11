df7afae1d7776cc2e42bc077e12c2508b72dae7f
khiropy <66423508+khiropy@users.noreply.github.com>
2020-06-11 19:50:52 +0200

    Create tribonacci.py

---
+++ tribonacci.py
@@ -0,0 +1,13 @@
+def tribonacci(s,n):
+    rslt = [None] * n
+    rslt[0:3]=s
+    if n==0:
+       rslt=[]
+    elif n==1:
+         rslt=s[0:1]
+    else:
+        for i in range(3, n):
+           rslt[i] = rslt[i - 2] + rslt[i - 1] + rslt[i - 3]
+    print(rslt)
+    return
+tribonacci([1,1,1],10)
